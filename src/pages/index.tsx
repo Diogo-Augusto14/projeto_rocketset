f02dd1a634f504c2a0f9ec83c8d15a76785a8666
Diogo Augusto Oliveira das Graças <diogo.gracas@pemill.com.br>
2026-05-08 16:38:59 -0300

    feat: remover guia de dados em PDF e atualizar layout da página inicial
    - Removido o arquivo guia_dados.pdf.
    - Atualizado o layout da página inicial para incluir uma seção de perfil com foto e redes sociais.
    - Adicionadas novas imagens SVG para redes sociais (Instagram, LinkedIn, Facebook, GitHub).
    - Alterada a cor de fundo da aplicação para um gradiente diferente.
    - Adicionada nova imagem de perfil.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,44 @@
-import { Header } from "@/components/header";
-import { HeroSection } from "@/components/Hero/hero";
-import { Button } from "@/components/ui/button";
-
 export default function Page() {
   return (
-    <div className="">
-       
+    <div className="grid grid-cols-4 justify-center mt-[50px]">
+      <div className="col-span-1"></div>
+
+      {/* Coluna da Foto de Perfil */}
+      <div className="col-span-1 p-4 flex flex-col items-center justify-center gap-4 mr-[90px]">
+        <img
+          src="/perfil.png"
+          alt="Foto de Perfil"
+          className="w-full h-full rounded-full shadow-lg"
+        />
+        <p className="text-center font-medium text-white">
+          Diogo Augusto Oliveira das Graças
+        </p>
+      </div>
+
+      
+      <div className="col-span-1   p-6 rounded-lg flex flex-col gap-8 text-white">
+        
+        
+        <div className="flex flex-col gap-3">
+          <p className="font-bold">Redes Sociais:</p>
+          <div className="flex flex-wrap gap-4">
+            <img src="/github.svg" alt="Github" className="w-8 h-8 rounded-full shadow-lg hover:scale-110 transition-transform cursor-pointer" />
+            <img src="/facebook.svg" alt="Facebook" className="w-8 h-8 rounded-full shadow-lg hover:scale-110 transition-transform cursor-pointer" />
+            <img src="/instragam.svg" alt="Instagram" className="w-8 h-8 rounded-full shadow-lg hover:scale-110 transition-transform cursor-pointer" />
+            <img src="/Linkend.svg" alt="Linkedin" className="w-8 h-8 rounded-full shadow-lg hover:scale-110 transition-transform cursor-pointer" />
+          </div>
+        </div>
+
+        
+        <div className="flex flex-col gap-2">
+          <p className="text-2xl font-bold">
+            Resumo Profissional:
+          </p>
+          <p className="text-gray-200">Programador júnior em início de carreira</p>
+        </div>
+      </div>
+
+      <div className="col-span-1"></div>
     </div>
   );
 }
